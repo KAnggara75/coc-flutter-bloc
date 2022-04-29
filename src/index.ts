629fe4f9320885cc0f7bff852baa835c56df7d96
Call Vin <kanggara75@gmail.com>
2022-04-29 16:53:56 +0700

    port from https://github.com/felangel/bloc/tree/master/extensions/vscode

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,29 @@
-import { commands, CompleteResult, ExtensionContext, listManager, sources, window, workspace } from 'coc.nvim';
-import DemoList from './lists';
+import { commands, ExtensionContext, languages, workspace } from 'coc.nvim';
+import { analyzeDependencies } from './utils';
+import { newBloc, newCubit } from './commands';
+import { BlocCodeActionProvider } from "./code-actions";
+
+import {
+  newBloc,
+  newCubit,
+  // convertToMultiBlocListener,
+  // convertToMultiBlocProvider,
+  // convertToMultiRepositoryProvider,
+  // wrapWithBlocBuilder,
+  // wrapWithBlocListener,
+  // wrapWithBlocConsumer,
+  // wrapWithBlocProvider,
+  // wrapWithRepositoryProvider,
+  // wrapWithBlocSelector,
+} from "./commands";
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  window.showMessage(`coc-flutter-bloc works!`);
+  if (workspace.getConfiguration('bloc').get<boolean>('checkForUpdates')) {
+    analyzeDependencies();
+  }
 
   context.subscriptions.push(
-    commands.registerCommand('coc-flutter-bloc.Command', async () => {
-      window.showMessage(`coc-flutter-bloc Commands works!`);
-    }),
-
-    listManager.registerList(new DemoList(workspace.nvim)),
-
-    sources.createSource({
-      name: 'coc-flutter-bloc completion source', // unique id
-      doComplete: async () => {
-        const items = await getCompletionItems();
-        return items;
-      },
-    }),
-
-    workspace.registerKeymap(
-      ['n'],
-      'flutter-bloc-keymap',
-      async () => {
-        window.showMessage(`registerKeymap`);
-      },
-      { sync: false }
-    ),
-
-    workspace.registerAutocmd({
-      event: 'InsertLeave',
-      request: true,
-      callback: () => {
-        window.showMessage(`registerAutocmd on InsertLeave`);
-      },
-    })
+    commands.registerCommand('bloc.new-bloc', newBloc),
+    commands.registerCommand('bloc.new-cubit', newCubit),
   );
-}
-
-async function getCompletionItems(): Promise<CompleteResult> {
-  return {
-    items: [
-      {
-        word: 'TestCompletionItem 1',
-        menu: '[coc-flutter-bloc]',
-      },
-      {
-        word: 'TestCompletionItem 2',
-        menu: '[coc-flutter-bloc]',
-      },
-    ],
-  };
 }
